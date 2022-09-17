b53f31cce5e3463649055ae0df0eba69d7c3e12d
Innovanito <khg05311@naver.com>
2022-09-17 17:58:05 +0900

    38:10 using icon error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  
+    return () => {
+      
+    };
+  }, [])
   return (
-    <View>
-      <Text className='text-red-300'>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className='text-red-600'>
+        {/* Header */}
+        <View className=' flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image
+            source={{
+              url: "https://links.papareact.com/wru"
+            }}
+            className=" h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className='font-bold text-gray-400 text-xs'>Deliver now!</Text>
+            <Text className='font-bold text-xl '>
+              Current Location 2
+              {/* <ChevronDownIcon size={20}  /> */}
+            </Text>
+          </View>
+
+          <UserIcon size={35} />
+        </View>
+
+      </Text>
+    </SafeAreaView>
   )
 }
 

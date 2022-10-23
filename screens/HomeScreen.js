07bfad2bb9c60744bcd8edde180e37ad229fd0e0
Innovanito <khg05311@naver.com>
2022-10-23 14:24:02 +0900

    44:33

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
@@ -21,28 +21,41 @@ const HomeScreen = () => {
     };
   }, [])
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className='text-red-600'>
         {/* Header */}
         <View className=' flex-row pb-3 items-center mx-4 space-x-2'>
           <Image
             source={{
               url: "https://links.papareact.com/wru"
-            }}
+            }} 
             className=" h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-
-          <View>
-            <Text className='font-bold text-gray-400 text-xs'>Deliver now!</Text>
+          <View className='flex-1'>
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver now!
+            </Text>
             <Text className='font-bold text-xl '>
-              Current Location 2
-              {/* <ChevronDownIcon size={20}  /> */}
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB"  />
             </Text>
           </View>
 
-          <UserIcon size={35} />
+          <UserIcon size={35} color="#00CCBB" />
         </View>
 
+        {/* Search */}
+        <View className='flex-row items-center px-2 mx-4 space-x-4 '>
+          <View className='flex-row flex-1 space-x-2 bg-gray-200'>
+            <TextInput
+              placeholder='Search Restuarants and cuisines'
+              keyboardType='default'
+            />
+          </View>
+          <Text className='text-blue-300'>
+            Adjustment
+          </Text>
+        </View>
       </Text>
     </SafeAreaView>
   )

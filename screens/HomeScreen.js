e09f93d103b3b5f1ea5fc4bd6b9f6ac02f272252
Innovanito <khg05311@naver.com>
2022-10-25 18:21:26 +0900

    54:59

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, {useLayoutEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
@@ -7,6 +7,8 @@ import {
   SearchIcon,
   AdjustmentsIcon
 } from "react-native-heroicons/outline"
+
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -22,9 +24,8 @@ const HomeScreen = () => {
   }, [])
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className='text-red-600'>
         {/* Header */}
-        <View className=' flex-row pb-3 items-center mx-4 space-x-2'>
+        <View className=' flex-row pb-3 items-center mx-4 space-x-2 '>
           <Image
             source={{
               url: "https://links.papareact.com/wru"
@@ -45,8 +46,8 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className='flex-row items-center px-2 mx-4 space-x-4 '>
-          <View className='flex-row flex-1 space-x-2 bg-gray-200'>
+        <View className='flex-row items-center space-x-2 px-2'>
+          <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
             <TextInput
               placeholder='Search Restuarants and cuisines'
               keyboardType='default'
@@ -56,7 +57,13 @@ const HomeScreen = () => {
             Adjustment
           </Text>
         </View>
-      </Text>
+
+        {/* Body */}
+        <ScrollView className='bg-red-400'>
+          {/* Categories */}
+            <Categories/>
+          {/* Featured Rows */}
+        </ScrollView>
     </SafeAreaView>
   )
 }

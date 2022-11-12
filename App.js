e6e1d0e116fd9317207643b40f08dd676b9d8c9c
Innovanito <khg05311@naver.com>
2022-11-12 17:20:50 +0900

    2:36

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ResturantScreen from './screens/ResturantScreen';
+import { Provider } from 'react-redux';
+import {store} from './store'
 
 const Stack = createNativeStackNavigator()
 
@@ -10,12 +12,14 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Resturant" component={ResturantScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Resturant" component={ResturantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
